81a365271a1a12dcd1ec1157a98154496bda4650
Kelly <kelly.shpark@gmail.com>
2020-11-09 21:56:18 -0500

    edit from beautifulsoup

--- coronaMapKR.py
+++ coronaMapKR.py
@@ -10,7 +10,13 @@ page = page.read()
 
 # BeautifulSoup 을 활용하여 데이터 가져옴
 soup = BeautifulSoup(page, 'html.parser')
-seoul_gu = soup.find_all('h5', 'update-date')[1]
+seoul_gu = soup.find('div', {'class':'status-confirm'})
+corona_gu_total = seoul_gu.get_text()
+print(corona_gu_total)
+
+# 구 리스트, 확진자 수 리스트 -> 합치기
+
+'''
 corona_gu_total = seoul_gu.get_text()
 corona_gu_total = corona_gu_total.split('(')[1]
 corona_gu_total = corona_gu_total.split()[0:25]
@@ -18,6 +24,10 @@ corona_gu_total
 
 # 2) 데이터 가공
 corona_df = pd.DataFrame(corona_gu_total)
+
+corona_df.head()
+
+
 corona_df = corona_df.rename(columns={0 : 'seoul_gu'})
 
 corona_df['num_corona'] = corona_df['seoul_gu'].str.extract('(\d+)')
@@ -26,16 +36,21 @@ corona_df['seoul_gu'] = corona_df['seoul_gu'].str.extract('(\D+)')
 corona_df['seoul_gu'] = corona_df['seoul_gu'] + '구'
 corona_df['seoul_gu'][23] = '중구'
 
-# 구별 경계선 json 파일
-geo_data = '' # shp extension -> json extension
 
-'''
-with open(geo_data, encoding='utf-8') as f:
-    data = json.load(f.read())
+# 구별 경계선 json 파일
+geo_data = 'C:/Users/SEHEE/Downloads/pyVisualization/seoul.json' 
+# shp extension -> json extension
+
+
+with open(geo_data,encoding='utf-8') as f:
+    data = json.loads(f.read())
 
 # 행정 구역 경계에 구별 코로나 감염자 수 추가
-for i in range(len(corona_df)):
-    for j in range(len(corona_df)) :
-        if data['features'][i]['properties']['name'] == corona_df['seoul_gu'][j]:
-            data['features'][i]['properties'['corona'] = int(corona_df['num_corona'][j])
+for i in range(len(corona_df)) : 
+    for j in range(len(corona_df)) : 
+        if data['features'][i]['properties']["name"] == corona_df["seoul_gu"][j] : 
+            data['features'][i]['properties']["corona"] = int(corona_df["num_corona"][j])
+
+
+corona_df.head()
 '''
